6401a69998046b1d04b13deba74029a155364161
eight <e1537@s.akashi.ac.jp>
2016-01-28 19:26:31 +0900

    最初のコミット

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour 
+{
+    void OnCollisionEnter (Collision hit)
+    {
+        if (hit.gameObject.CompareTag ("Player"))
+        {
+            SceneManager.LoadScene("unity");
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    void FixedUpdate ()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(x * speed, 0, z * speed);
+    }
+}
